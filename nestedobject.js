2d6e714699f90fa384b2a6b9d9495c2cd50ecf28
sumanth-cs <108050388+sumanth-cs@users.noreply.github.com>
2022-07-02 10:02:21 +0530

    Add files via upload

---
+++ nestedobject.js
@@ -0,0 +1,23 @@
+const users ={
+    anand : {
+        employee_ID : 1001,
+        salary : 30000,
+        phoneNumber : 9274671232,
+        address : "bangalore",
+    },
+    chandan : {
+        employee_ID : 1002,
+        salary :31000,
+        phoneNumber : 9876542342,
+        address : "chennai",
+    },
+    farhan : {
+        employee_ID : 1003,
+        salary : 32991,
+        phoneNumber : 3456789987,
+        address : 'hyderbad',
+    },
+}
+console.log(users);
+console.log("Anand phone number is " + users.anand.phoneNumber);
+console.log("Farhan employee ID is " + users.farhan.employee_ID);
